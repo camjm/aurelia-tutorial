be3160e33957bcdcce88d596c60ec9f661476044
Cam <2276730+camjm@users.noreply.github.com>
2024-08-31 17:15:25 +1200

    event bindings

--- src/hello-name.ts
+++ src/hello-name.ts
@@ -1,3 +1,59 @@
 export class HelloName{
   public name = 'Person';
+  public agree = false;
+  public items = [];
+
+  constructor() {
+    this.items.push({
+      id: 1,
+      name: 'Cam'
+    });
+  }
+
+  public divClicked() {
+    console.log('divClicked');
+  }
+
+  public buttonClicked() {
+    console.log('buttonClicked');
+  }
+
+  public onAgreementChange() {
+    console.log(this.agree);
+  }
+
+  public handleKeydown(event) {
+    switch (event.key) {
+      case 'Enter':
+        console.log('Enter');
+        break;
+      case 'Escape':
+        console.log('Escape');
+        break;
+      default:
+        console.log('Other');
+        break;
+    }
+  }
+
+  public listClicked(event) {
+    const itemId = event.target.getAttribute('data-id');
+    if (itemId) {
+      // Logic to handle the click event on an item
+      console.log(itemId);
+    }
+  }
+
+  public search(query) {
+    console.log(query);
+  }
+
+  public onCtrlClick() {
+    console.log('onCtrlClick');
+  }
+
+  public send() {
+    console.log('Send');
+  }
+
 }
